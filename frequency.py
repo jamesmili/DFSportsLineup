905cc226defcb93c2fba5c1837af5190a6298772
James Militante <james.militante@gmail.com>
2020-03-08 16:16:09 -0400

    read multiple files

--- frequency.py
+++ frequency.py
@@ -1,5 +1,4 @@
-import csv
-from collections import OrderedDict
+import csv, sys, os
 
 class Frequency:
     def __init__(self, placements):
@@ -25,42 +24,47 @@ class Frequency:
         return players
 
     def addPlayer(self, player):
-        if player[0] in self.PLAYERS:
-            self.PLAYERS[player[0]]['frequency'] += 1
+        if player[0] not in self.PLAYERS:
+            self.PLAYERS[player[0]] =   { 
+                                            'frequency': 0,
+                                            'drafted': player[2],
+                                            'average_points': float(player[3])
+                                        }
         else:
-            self.PLAYERS[player[0]] = { 
-                                        'frequency': 0,
-                                        'pos': player[1],
-                                        'drafted': player[2],
-                                        'total_points': player[3]
-                                    }
+            average_points = round((self.PLAYERS[player[0]]['average_points'] + float(player[3])) / 2, 2)
+            self.PLAYERS[player[0]]['average_points'] = average_points
 
-    def read(self):
-        with open('contest-standings-86385131.csv') as csv_file:
+    def addPlayerFrequency(self, player):
+        if player in self.TOPPLAYERS:
+            self.TOPPLAYERS[player]['frequency'] += 1
+
+    def read(self, path):
+        print("reading files from: " + path)
+        self.LINEUPS=[]
+        with open(path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             #skips header line
             csv_reader.__next__()
-            counter = 1
             for row in csv_reader:
                 players = row[7::]
                 lineup = row[5]
                 if '' not in players:
                     self.addPlayer(players)
-                if counter is not self.PLACEMENTS:
+                if len(lineup) is not 0:
                     self.LINEUPS.append(self.filterLineup(lineup))
-                    counter += 1
+                else: break
         self.TOPPLAYERS = self.PLAYERS
+        self.calculate()
         
-    def calculate(self, num=None):
-        print(num)
+    def calculate(self):
         self.TOPPLAYERS = self.PLAYERS
-        if num is not None:
-            lineups = self.LINEUPS[num-1]
+        if self.PLACEMENTS is not 0:
+            lineups = self.LINEUPS[0:self.PLACEMENTS]
         else:
             lineups = self.LINEUPS
         for lineup in lineups:
             for player in lineup:
-                self.TOPPLAYERS[player]['frequency'] += 1
+                self.addPlayerFrequency(player)
 
     def getPlayers(self):
         return self.TOPPLAYERS
@@ -69,11 +73,13 @@ class Frequency:
         return self.LINEUPS
     
     def getFrequentPlayers(self):
-        return {k: v for k, v in sorted(self.TOPPLAYERS.items(), key=lambda item: item[1]['frequency'], reverse=True)}
+        return {k: v for k, v in sorted(self.TOPPLAYERS.items(), key=lambda item: item[1]['frequency'], reverse=True) if v['frequency'] is not 0}
 
 
 if __name__ == "__main__":
-    f = Frequency(10)
-    f.read()
-    f.calculate()
+    path = sys.argv[1]
+    placements = int(sys.argv[2])
+    f = Frequency(placements)
+    for files in os.listdir(path):
+        f.read(path+"/"+files)
     print(f.getFrequentPlayers())
